df68577f9e9384533d8c2e4231fc0c9fefb9414e
SantiagoVillacis <savihuer@espol.edu.ec>
2025-05-29 20:12:58 -0500

    Create Compra.java

---
+++ Compra.java
@@ -0,0 +1,21 @@
+public class Compra{
+    private Pago pago;
+    private PagoPayPal pagoPayPal;
+    private List articulos;
+
+    public Compra(Pago pago){
+        //Inicializacion
+    }
+
+    public Compra(PagoPayPal pagoPayPal){
+        //Inicializacion
+    }
+
+    public void agregarArticulo(Articulo articulo){
+        //Agregar un articulo a la compra
+    }
+
+    public void removerArticulo(Articulo articulo){
+        //Remover un articulo de la compra
+    }
+}
