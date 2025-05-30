12b798eda59a7a391d23f8e951bb795f0becad82
LisbethLlamuca <lisbllam@espol.edu.ec>
2025-05-29 20:37:04 -0500

    Update PagoPayPal.java

--- PagoPayPal.java
+++ PagoPayPal.java
@@ -1,10 +1,14 @@
 public class PagoPayPal extends Pago{
   private boolean loggedIn; //Conexion a cuenta PayPal
+
+  public static void iniciarSesion(){
+    this.loggedIn = true;
+  }
   
   @Override
   public void realizarCobro(double monto){
     if(!loggedIn){
-      return;
+      inciarSesion();
     }
     //Cargar el monto de compra al medio de pago
   }
